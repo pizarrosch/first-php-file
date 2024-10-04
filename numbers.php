31710863692fd794c89f17ccd0f8ea82df6b04a2
pizarrosch <bayernsch89@gmail.com>
2024-10-05 01:44:53 +0200

    Use numbers

---
+++ numbers.php
@@ -0,0 +1,16 @@
+<?php include 'error.php'?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Numbers</title>
+	<link rel="stylesheet" href="styles/global.css" type="text/css" />
+</head>
+<body>
+	<?php
+		echo '13' + '11';
+		echo '<br>';
+		echo round(3.12457509902, 3);
+	?>
+</body>
+</html>
