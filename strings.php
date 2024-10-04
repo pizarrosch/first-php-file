07997ca8d7fc8229d31dfbaba457b6024919ce47
pizarrosch <bayernsch89@gmail.com>
2024-10-05 01:18:33 +0200

    Use strings

---
+++ strings.php
@@ -0,0 +1,46 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="styles/global.css" type="text/css"></link>
+	<title>Document</title>
+</head>
+<body>
+<div>
+	<?php
+		$greeting = "I'm a string";
+		echo $greeting . ' !!!';
+		
+		echo '<br>';
+		$name = 'Zaur';
+		$subject = 'PHP';
+		//difference between single and double quotes
+		echo 'Hello, I\'m ' . $name . ' and I am learning ' . $subject . '!';
+		echo '<br>';
+		echo "Hello, I'm $name and I am learning $subject!";
+		
+		$string = "const name = 'Zaur';
+		
+		if (name === 'Zaur') {
+			console.log('Hello, Zaur!');
+		}";
+		
+		$heredoc = <<<HEREDOC
+		"const name = 'Zaur';
+		
+		if (name === 'Zaur') {
+			console.log('Hello, Zaur!');
+		}";
+		HEREDOC;
+
+//		echo '<br>';
+//		echo $string;
+//		echo '<br>';
+//		echo $string;
+		echo '<br>';
+	?>
+	
+	<textarea><?php echo $heredoc; ?></textarea>
+</div>
+</body>
+</html>
